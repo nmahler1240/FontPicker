39d976cf8bda94d7984f5a694f897dded8a8720f
nmahler1240 <81329454+nmahler1240@users.noreply.github.com>
2021-05-02 17:02:18 +0200

    Add files via upload

---
+++ fonts.js
@@ -0,0 +1,2 @@
+const = ["Arial, sans-serif", "Helvetica, sans-serif", "Verdana, sans-serif", "Trebuchet MS, sans-serif", "Gill Sans, sans-serif", "Noto Sans, sans-serif", "Avantgarde, TeX Gyre Adventor, URW Gothic L, sans-serif", "Times, Times New Roman, serif", "Georgia, serif", "Andale Mono, monospace", "Courier, monospace"];
+//11fonts
